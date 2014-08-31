ff039a6cad2e6fe30c22f3dee6f71f29970d3fe6
Leo Chen-Tsu_Lin <lin19890808@gmail.com>
2014-09-01 02:18:13 +0800

    success send bytes

--- TCPSocket/ViewController.swift
+++ TCPSocket/ViewController.swift
@@ -31,13 +31,17 @@ class ViewController: UIViewController, NSStreamDelegate, UITextFieldDelegate  {
     
     override func viewDidAppear(animated: Bool) {
         
+        println(1)
         var alert = UIAlertController(title: "Alert", message: "Message", preferredStyle: UIAlertControllerStyle.Alert)
         
         alert.addAction(UIAlertAction(title: "Click", style: UIAlertActionStyle.Default, handler: {(alertAction: UIAlertAction!) in
             
-            println(alert.textFields[0].text)
-            println(alert.textFields[1].text)
-            self.connectToSocket(alert.textFields[0].text, port: alert.textFields[1].text)
+            let ipTextField   = alert.textFields[0] as UITextField
+            let portTextField = alert.textFields[1] as UITextField
+            
+            println(ipTextField.text)
+            println(portTextField.text)
+            self.connectToSocket(ipTextField.text, port: portTextField.text.toInt()!)
         
         }))
         
@@ -50,17 +54,38 @@ class ViewController: UIViewController, NSStreamDelegate, UITextFieldDelegate  {
         self.presentViewController(alert, animated: true, completion: nil)
     }
     
-    func connectToSocket(host: String, port: String) {
+    func connectToSocket(host: String, port: Int) {
     
         var input : NSInputStream?
         var output: NSOutputStream?
         
-        var socket: NSStream = NSStream.getStreamstoHost(NSHost.init(host), port, &input, &output)
+        println(host)
+        println(port)
+        
+        NSStream.getStreamsToHostWithName(host, port: port, inputStream: &input, outputStream: &output)
         
         let str = "test"
-        let data = str.dataUsingEncoding(NSUTF8StringEncoding, allowLossyConversion: false)
+        let data = str.dataUsingEncoding(NSUTF8StringEncoding, allowLossyConversion: false)!
+        
+        println(data)
+        println(data.length)
+        
+        output?.open()
+
 
         let bytesWritten = output!.write(UnsafePointer(data.bytes), maxLength: data.length)
+        
+        println(bytesWritten)
+        
+//        let serverAddress: CFString = host as NSString
+//        let serverPort: UInt32 = UInt32(port)
+//        
+//        var readStream: Unmanaged<CFReadStream>?
+//        var writeStream: Unmanaged<CFWriteStream>?
+//        
+//        CFStreamCreatePairWithSocketToHost(kCFAllocatorDefault, serverAddress, serverPort, &readStream, &writeStream)
+//        CFReadStreamOpen(readStream)
+//        CFWriteStreamOpen(writeStream)
         
     }
     
